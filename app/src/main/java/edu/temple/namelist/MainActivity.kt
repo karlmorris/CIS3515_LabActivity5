18b30a2fa9a1a8a9755a644085a49bfc80cb8f4d
NateScott28 <tul07731@temple.edu>
2024-03-13 15:45:19 -0400

    fixed empty list bug

--- app/src/main/java/edu/temple/namelist/MainActivity.kt
+++ app/src/main/java/edu/temple/namelist/MainActivity.kt
@@ -32,6 +32,7 @@ class MainActivity : AppCompatActivity() {
                 }
 
                 override fun onNothingSelected(p0: AdapterView<*>?) {
+                    nameTextView.text = "Empty List"
                 }
             }
         }
