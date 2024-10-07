a234895ade0bbf41844b4714ce2ca95fdd47969b
Tony-Elia <antony.safaga@gmail.com>
2024-10-07 15:54:39 -0400

    fix bug1

--- app/src/main/java/edu/temple/namelist/CustomAdapter.kt
+++ app/src/main/java/edu/temple/namelist/CustomAdapter.kt
@@ -9,9 +9,7 @@ import android.widget.TextView
 class CustomAdapter(private val names: List<String>, private val context: Context) : BaseAdapter() {
 
     // How many items are in the collection
-    override fun getCount(): Int {
-        return 5
-    }
+    override fun getCount() = names.size
 
     // Fetch an item from the collection
     override fun getItem(p0: Int): Any {
