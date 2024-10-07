4ea72d9a6dbe5da2547843a1dbb6791494b27462
mariam0ashraf <mariam00ashraf@gmail.com>
2024-10-07 16:10:59 -0400

    Fixing The bugs

--- app/src/main/java/edu/temple/namelist/MainActivity.kt
+++ app/src/main/java/edu/temple/namelist/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+//
         names = mutableListOf("Kevin Shaply", "Stacey Lou", "Gerard Clear", "Michael Studdard", "Michelle Studdard")
 
         val spinner = findViewById<Spinner>(R.id.spinner)
