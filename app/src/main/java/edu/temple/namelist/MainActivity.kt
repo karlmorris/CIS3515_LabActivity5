0c1810cd9526b91cb4c1b57faf08f2d99ec071dc
srujankd <srujan.damaraju@temple.edu>
2025-10-13 15:28:28 -0400

    once a person is deleted, the TextView in the middle of the screen also reflects the new person that is auto displayed in the spinner

--- app/src/main/java/edu/temple/namelist/MainActivity.kt
+++ app/src/main/java/edu/temple/namelist/MainActivity.kt
@@ -39,6 +39,16 @@ class MainActivity : AppCompatActivity() {
         findViewById<View>(R.id.deleteButton).setOnClickListener {
             (names as MutableList).removeAt(spinner.selectedItemPosition)
             (spinner.adapter as BaseAdapter).notifyDataSetChanged()
+
+            val pos = spinner.selectedItemPosition
+
+            if (names.isNotEmpty()) {
+                val newPos = minOf(pos, names.lastIndex)
+                spinner.setSelection(newPos)
+                nameTextView.text = names[newPos]
+            } else {
+                nameTextView.text = ""
+            }
         }
 
     }
