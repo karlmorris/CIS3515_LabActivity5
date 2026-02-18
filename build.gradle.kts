23db0f073df815f6e659815f94f93aadd712cb17
Frank Zhang <tup88114@temple.edu>
2026-02-18 15:21:17 -0500

    Finished
    fixed 2 bug

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,7 @@
+buildscript {
+    val agp_version by extra("8.7")
+    val agp_version1 by extra("8.6.1")
+}
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.6.1" apply false
